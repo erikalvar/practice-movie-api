a674e01167ab093161cdf0044530cee05f12565e
erikalvar <erikalvarbass@gmail.com>
2020-12-28 13:03:12 -0500

    adds title search variable to get and gsub if multiple words are input

--- app/controllers/api/searches_controller.rb
+++ app/controllers/api/searches_controller.rb
@@ -2,13 +2,15 @@ class Api::SearchesController < ApplicationController
   def index
     # search movies api by title
     title = params[:title]
+    # adds space if multiple words input
+    title = title.gsub(" ", "%20")
 
     response = HTTP
       .headers(
         "x-rapidapi-key" => Rails.application.credentials.movie_api[:api_key],
         "x-rapidapi-host" => "movies-tvshows-data-imdb.p.rapidapi.com",
       )
-      .get("https://movies-tvshows-data-imdb.p.rapidapi.com/?type=get-movies-by-title&title=matrix")
+      .get("https://movies-tvshows-data-imdb.p.rapidapi.com/?type=get-movies-by-title&title=#{title}")
 
     @movies = response.parse["movie_results"]
 
